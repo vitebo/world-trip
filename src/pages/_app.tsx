8606cd687bde294757868b6d5d257958af5107ee
vitebo <vitebo@hotmail.com>
2021-04-22 19:11:38 -0300

    chore(app): add chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
