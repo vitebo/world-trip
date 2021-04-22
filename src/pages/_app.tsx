e6d588c9b7a03c043a30274578f49bef0046773c
vitebo <vitebo@hotmail.com>
2021-04-22 20:42:56 -0300

    chore(chakra): create a basic theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { theme } from 'theme';
+
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
